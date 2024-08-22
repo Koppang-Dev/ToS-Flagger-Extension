1573203faf6fdf206f309894062b976aee049518
Riley <koppangdev@gmail.com>
2024-08-21 21:05:43 -0600

    Making dataframe for content and headers

--- summarize.py
+++ summarize.py
@@ -1,11 +1,12 @@
 # Importing modules
 from flask import Flask, request, jsonify
 import logging
+import pandas as pd
 
 # Setting up logging
 logging.basicConfig(level=logging.DEBUG)
 
-# Defining flask object
+# Defining flask objectpo
 app = Flask(__name__)
 @app.route('/process', methods=['POST']) # Sending data to the server
 
@@ -20,13 +21,17 @@ def process_data():
         content = data.get('content')
         headers = data.get('headers')
 
-        # Log the extracted content and headers
-        #logging.debug("Content: %s", content)
-        #logging.debug("Headers: %s", headers)
+        table_df = create_table(content, headers)
+
+        # Printing our dataframe
+        print("Data Frame: %s", table_df)
+
 
         # Summarizing the content
+
+        # 
         summarized_content = summarized_content(content)
-        logging.debug("Summary: %s", summarized_content)
+        #logging.debug("Summary: %s", summarized_content)
 
         # Response back to application
         return jsonify({
@@ -40,6 +45,11 @@ def process_data():
         logging.error("Error processing request: %s", str(e))
         return jsonify({"error", "Internal Server Error"}), 500
 
+
+def create_table(content, headers):
+    # Create a dataframe using the headers and content
+    df = pd.DataFrame([content], columns=headers)
+    return df
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port =8080, debug=True)

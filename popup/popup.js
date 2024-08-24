842b1a33738869ea9a5babf72c104228a3ebfa9c
Riley <koppangdev@gmail.com>
2024-08-24 13:32:00 -0600

    Made popup more pleasing UX

--- popup/popup.js
+++ popup/popup.js
@@ -3,22 +3,16 @@ const checkCounterElement = document.getElementById("checkedCount")
 
 // Button Elements
 const startButton = document.getElementById("startButton")
-const stopButton = document.getElementById("stopButton")
-
 
 startButton.onclick = () => {
     const prefs = {
         checkedCounter: checkCounterElement.textContent
     }
 
-    checkedCount += 1
 
     chrome.runtime.sendMessage({ event: 'onStart', prefs})
 }
 
-stopButton.onclick = () => { 
-    chrome.runtime.sendMessage({ event: 'onStop'})
-}
 
 // Retrieve saved data
 chrome.storage.local.get(["checkedCount"], (result) => {
